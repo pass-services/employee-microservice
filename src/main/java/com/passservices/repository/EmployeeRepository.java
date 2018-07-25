0e17270df1a0efe8c769eefbb25380c51da7559f
pass-services <35070791+pass-services@users.noreply.github.com>
2018-07-25 18:15:06 +0200

    Change package path

--- src/main/java/com/passservices/repository/EmployeeRepository.java
+++ src/main/java/com/passservices/repository/EmployeeRepository.java
@@ -2,7 +2,7 @@ package com.passservices.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.pass-services.model.Employee;
+import com.passservices.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
