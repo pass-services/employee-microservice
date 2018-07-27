d2308fafcc807f35c8f4f12061314915c7ed751a
Thebault <yannick@findmywines.com>
2018-07-27 23:02:31 +0200

    Add Temporal Type to entity

--- src/main/java/com/passservices/model/Employee.java
+++ src/main/java/com/passservices/model/Employee.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Employee {
@@ -15,6 +17,7 @@ public class Employee {
 
     private String firstName;
     private String lastName;
+    @Temporal(TemporalType.DATE)
     private Date birthDate;
     private String securityId;
 
